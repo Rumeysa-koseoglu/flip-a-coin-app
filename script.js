57c8116d8afa0d580399052f3647ebedc55ecdd5
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-03-14 02:47:43 +0300

    feat: add initial JS  structure

---
+++ script.js
@@ -0,0 +1,5 @@
+let heads = 0;
+let tails = 0;
+let coin = document.querySelectorAll(".coin");
+let flipBtn = document.querySelector("#flip-button");
+let resetBtn = document.querySelector("#reset-button");

164c575eb1154c3397658dd5e98a8a89e4d05ab3
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-03-15 03:31:22 +0300

    chore: add comments to CSS and JS files

--- script.js
+++ script.js
@@ -6,28 +6,34 @@ let resetBtn = document.querySelector("#reset-button");
 
 flipBtn.addEventListener("click", () => {
     let i = Math.floor(Math.random() * 2);
+    //reset the animation of the element
+    //  so that the animation works properly every time
     coin.style.animation = "none";
+    //if i == 1, run this 
     if (i) {
+        //start the "spin-heads" animation after 100ms with the settimeout function
         setTimeout(function () {
             coin.style.animation = "spin-heads 3s forwards";
         }, 100);
-        heads++;
-    } else {
+        heads++; //increase number of heads by 1
+    } else {  //if i == 0, run this
         setTimeout(function () {
             coin.style.animation = "spin-tails 3s forwards";
         }, 100);
         tails++;
     }
+    //since the animation lasts 3 seconds, call updateStats after 3 second
     setTimeout(updateStats, 3000);
     disableButton();
 });
+//update the number of heads and tails on the screen
 function updateStats() {
     document.querySelector("#heads-count").
         textContent = `Heads: ${heads}`;
     document.querySelector("#tails-count").
         textContent = `Tails: ${tails}`;
 }
-
+//temporarily disable button while animation is in progress
 function disableButton() {
     flipBtn.disabled = true;
     setTimeout(function () {
@@ -35,8 +41,11 @@ function disableButton() {
     }, 3000);
 }
 resetBtn.addEventListener("click", () => {
+    //reset the coin's rotation angle so that it returns to its original position
     coin.style.transform = "rotateX(0)";
+    //reset heads and tails counter
     heads = 0;
     tails = 0;
+    //reset the numbers displayed on the screen
     updateStats();
 });
